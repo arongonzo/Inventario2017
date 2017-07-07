a1298e6ee29f3dd0ead058068f652e9fd8ac6e8a
arongonzo <arongonzo@gmail.com>
2017-07-07 17:28:57 -0400

    actualizacion repuestos

--- proyecto/Inventario2017/src/Entidades/SolicitudRespuesto.java
+++ proyecto/Inventario2017/src/Entidades/SolicitudRespuesto.java
@@ -3,101 +3,85 @@ package Entidades;
 
 
 public class SolicitudRespuesto {
-    int id_repuesto;
-    int id_sistema;
+    int id_solicitudRepuesto;
+    int id_temporada;
+    String id_comercial;
     int id_unidad;
-    
-    String pista;
-    String marca;
-    String modelo;
-    String fecha;
-    
+    String fecha_creacion;
+    String nombre_tecnico;
+    String correo_electronico;
+    String Anexo;
+    int id_sistema;
+    String marca_equipo;
+    int id_condicionsistema;
+    String modelo_equipo;
+    String numero_Serie;
+    int id_fallacomponente;
+    String sintoma_falla;
+    String solicutus_orden_trabajo;
+    String fecha_solicitud;
+    int id_prioridad;
+    int id_prioridad_normal;
+    int id_propiedad_urgente;
     int estado;
+	
     
     public SolicitudRespuesto() {
     }
 
-    public SolicitudRespuesto(int id_repuesto, 
-            int id_sistema, 
-            int id_unidad, 
-            String pista,
-            String marca,
-            String modelo,
-            String fecha,
-            int estado) 
+    public SolicitudRespuesto(int id_solicitudRepuesto,
+    int id_temporada,
+    String id_comercial,
+    int id_unidad,
+    String fecha_creacion,
+    String nombre_tecnico,
+    String correo_electronico,
+    String Anexo,
+    int id_sistema,
+    String marca_equipo,
+    int id_condicionsistema,
+    String modelo_equipo,
+    String numero_Serie,
+    int id_fallacomponente,
+    String sintoma_falla,
+    String solicutus_orden_trabajo,
+    String fecha_solicitud,
+    int id_prioridad,
+    int id_prioridad_normal,
+    int id_propiedad_urgente,
+    int estado) 
     {
-        this.id_repuesto = id_repuesto;
-        this.id_sistema = id_sistema;
+        this.id_solicitudRepuesto = id_solicitudRepuesto;
+        this.id_temporada = id_temporada;
+        this.id_comercial = id_comercial;
         this.id_unidad = id_unidad;
-        this.pista = pista;
-        this.marca = marca;
-        this.modelo = modelo;
-        this.fecha = fecha;
+        this.fecha_creacion = fecha_creacion;
+        this.nombre_tecnico = nombre_tecnico;
+        this.correo_electronico = correo_electronico;
+        this.Anexo = Anexo;
+        this.id_sistema = id_sistema;
+        this.marca_equipo = marca_equipo;
+        this.id_condicionsistema = id_condicionsistema;
+        this.modelo_equipo = modelo_equipo;
+        this.numero_Serie  = numero_Serie;
+        this.id_fallacomponente = id_fallacomponente;
+        this.sintoma_falla = sintoma_falla;
+        this.solicutus_orden_trabajo = solicutus_orden_trabajo;
+        this.fecha_solicitud = fecha_solicitud;
+        this.id_prioridad = id_prioridad;
+        this.id_prioridad_normal = id_prioridad_normal;
+        this.id_propiedad_urgente = id_propiedad_urgente;
         this.estado = estado;
     }
 
     
-    public int getIdRespuesto() {
-        return id_repuesto;
+    public int getIdSolicitudRepuesto() {
+        return id_solicitudRepuesto;
     }
     
-    public void setIdRespuesto(int id_repuesto) {
-        this.id_repuesto = id_repuesto;
+    public void setIdSolicitudRepuesto(int id_solicitudRepuesto) {
+        this.id_solicitudRepuesto = id_solicitudRepuesto;
     }
 
-    public int getIdSistema() {
-        return id_sistema;
-    }
-
-    public void setIdSistema(int id_sistema) {
-        this.id_sistema = id_sistema;
-    }
- 
-    public int getIdUnidad() {
-        return id_unidad;
-    }
-
-    public void setIdUnidad(int id_unidad) {
-        this.id_unidad = id_unidad;
-    }
     
-    public String getpista() {
-        return pista;
-    }
-
-    public void setpista(String pista) {
-        this.pista = pista;
-    }
-
-    public String getmarca() {
-        return marca;
-    }
-
-    public void setmarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getmodelo() {
-        return modelo;
-    }
-
-    public void setmodelo(String modelo) {
-        this.modelo = modelo;
-    }
-    
-    public String getfecha() {
-        return fecha;
-    }
-
-    public void setfecha(String fecha) {
-        this.fecha = fecha;
-    }
-    
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
 }
